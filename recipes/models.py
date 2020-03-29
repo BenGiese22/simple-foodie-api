50177fd35f89801d13a7bd43c20916877c62f019
Benjamin Giese <bengiese22@gmail.com>
2020-03-29 12:52:15 -0500

    increase field char size2

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Recipe(models.Model):
     link = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    ingredients = models.CharField(max_length=300)
+    ingredients = models.CharField(max_length=3000)
     directions = models.CharField(max_length=4000)
     source = models.CharField(max_length=50)
     created_date = models.DateTimeField('date published')
