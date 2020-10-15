f11c78ad6b440d484e263ea7f1ee87097c186e2f
Benjamin Giese <bengiese22@gmail.com>
2020-10-15 17:38:22 -0600

    fix migration syntax

--- recipes/migrations/0002_add_image_and_time_fields.py
+++ recipes/migrations/0002_add_image_and_time_fields.py
@@ -5,8 +5,6 @@ class Migration(migrations.Migration):
     dependencies = [('migrations', '0001_initial')]
 
     operations = [
-        # migrations.DeleteModel('Tribble'),
-        # migrations.AddField('image', 'rating', models.IntegerField(default=0)),
-        migrations.AddField('image', models.CharField(max_length=5000, blank=True),
-        migrations.AddField('total_prep_time', models.IntegerField(blank=True, null=True),
+        migrations.AddField('image', 'image', models.CharField(max_length=5000, blank=True),
+        migrations.AddField('total_prep_time', 'total_prep_time', models.IntegerField(blank=True, null=True),
     ]
